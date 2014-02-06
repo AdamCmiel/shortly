7be387e5c16ae682bce64cf8be17e964cbbfea35
Hack Reactor <hackreactor@HR-A4.local>
2014-02-05 20:32:58 -0800

    Client side username and password form submission completed

--- public/client/app.js
+++ public/client/app.js
@@ -2,24 +2,34 @@ window.Shortly = Backbone.View.extend({
 
   template: _.template(' \
       <h1>Shortly</h1> \
+      <div class="divLogin"></div> \
       <div class="navigation"> \
       <ul> \
         <li><a href="#" class="index">All Links</a></li> \
         <li><a href="#" class="create">Shorten</a></li> \
+        <li><a href="#" class="login">Login</a></li> \
+        <li><a href="#" class="logout">Logout</a></li> \
       </ul> \
       </div> \
       <div id="container"></div>'
   ),
 
+  loginTemplate: _.template('<form class="loginForm"><label>Username</label><input class="username" type="text" placeholder="username..."></input><input class="password" type="text" placeholder="****"></input><input type="submit" id="submitButton" value="login"></input></form>'),
+
   events: {
     "click li a.index":  "renderIndexView",
-    "click li a.create": "renderCreateView"
+    "click li a.create": "renderCreateView",
+    "click li a.login": "renderLoginView",
+    "click  #submitButton": "postLogin"
+    // "submit form.login": "postLogin"
+    // "click li a.logout": "render"
   },
 
   initialize: function(){
     console.log( "Shortly is running" );
     $('body').append(this.render().el);
     this.renderIndexView(); // default view
+    //$('form.login').submit(this.postLogin);
   },
 
   render: function(){
@@ -32,6 +42,8 @@ window.Shortly = Backbone.View.extend({
     var links = new Shortly.Links();
     var linksView = new Shortly.LinksView( {collection: links} );
     this.$el.find('#container').html( linksView.render().el );
+    $('.divLogin').append(this.loginTemplate());
+
     this.updateNav('index');
   },
 
@@ -47,6 +59,21 @@ window.Shortly = Backbone.View.extend({
             .removeClass('selected')
             .filter('.'+className)
             .addClass('selected');
+  },
+
+  renderLoginView: function(e){
+    e && e.preventDefault();
+  },
+
+  postLogin: function(e){
+    e && e.preventDefault();
+    var username = $('.username').val();
+    var password = $('.password').val();
+    var postData = {
+      username: username,
+      password: password
+    };
+    $.post('/users/create', JSON.stringify(postData), function(something){console.log(something)});
   }
 
 });
